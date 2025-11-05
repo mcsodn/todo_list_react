e48b4b705343ffcc1839a118106e30d48b0c7067
Однолетков Максим Михайлович <odnoletkov.maksim@itmh.ru>
2025-11-05 21:48:33 +0500

    fix many task header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import BoxIcon from "../assets/icons/BoxIcon";
 function Header() {
   return (
     <div
-      className="w-full h-[60px] border-b-2 border-sec-gray flex justify-center items-center gap-2
+      className="w-full border-b-2 border-sec-gray flex justify-center items-center gap-2 p-4
 "
     >
       <h1 className=" text-2xl text-main-gray">Мои задачи</h1>
