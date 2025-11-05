038ea1a26ea0cb2a4e7d7cfe387e50a4024457cb
Однолетков Максим Михайлович <odnoletkov.maksim@itmh.ru>
2025-11-05 18:16:36 +0500

    upd Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,13 @@
-import AddIcon from "../assets/icons/AddIcon";
 import BoxIcon from "../assets/icons/BoxIcon";
 
 function Header() {
   return (
     <div
-      className="w-full h-[60px] border-b-2 border-sec-gray flex justify-between items-center
+      className="w-full h-[60px] border-b-2 border-sec-gray flex justify-center items-center gap-2
 "
     >
-      <BoxIcon className="w-8 ml-4" />
-      <h1 className=" text-2xl text-main-gray">Пакет с задачами</h1>
-      <AddIcon className="w-8 mr-4" />
+      <h1 className=" text-2xl text-main-gray">Мои задачи</h1>
+      <BoxIcon className="w-6" />
     </div>
   );
 }
